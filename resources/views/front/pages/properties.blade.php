51e5987e76c04f7539878882a10977cce3bd711f
hamzaakhtar901 <135383531+hamzaakhtar901@users.noreply.github.com>
2023-06-03 04:53:01 +0500

    picture work and fixes (#3)
    Co-authored-by: sheikh muhammad <sheikh.muhammad@olivedigital.com>

--- resources/views/front/pages/properties.blade.php
+++ resources/views/front/pages/properties.blade.php
@@ -48,74 +48,75 @@
                     </select>
                     <div class="search-group" style="width: 200px !important;">
                         <i class="fas fa-dollar"></i>
-                        <input style="min-width: 165px !important;" type="text" id="price_range_field" class="form-control" placeholder="Search here"
+                        <input style="min-width: 165px !important;" type="text" id="price_range_field"
+                               class="form-control" placeholder="Search here"
                                name="price_range"
                                value="{{ request()->get('price_range') }}">
                     </div>
-{{--                    <select name="price_range" id="" class="form-control select2">--}}
-{{--                        <option value="">Price Range</option>--}}
-{{--                    </select>--}}
-{{--                    <div class="dropdown">--}}
-{{--                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"--}}
-{{--                                aria-haspopup="true" aria-expanded="false">--}}
-{{--                            <i class="fas fa-sliders-h"></i> More Filters--}}
-{{--                        </button>--}}
-{{--                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">--}}
-{{--                            <label>Square Feet</label>--}}
-{{--                            <div class="d-flex align-items-center">--}}
-{{--                                <select name="" id="" class="form-control">--}}
-{{--                                    <option value="">Any Beds</option>--}}
-{{--                                </select>--}}
-{{--                                <span>to</span>--}}
-{{--                                <select name="" id="" class="form-control">--}}
-{{--                                    <option value="">Any Beds</option>--}}
-{{--                                </select>--}}
-{{--                            </div>--}}
-{{--                            <label>Lot Size</label>--}}
-{{--                            <div class="d-flex align-items-center">--}}
-{{--                                <select name="" id="" class="form-control">--}}
-{{--                                    <option value="">Any Beds</option>--}}
-{{--                                </select>--}}
-{{--                                <span>to</span>--}}
-{{--                                <select name="" id="" class="form-control">--}}
-{{--                                    <option value="">Any Beds</option>--}}
-{{--                                </select>--}}
-{{--                            </div>--}}
-{{--                            <label>Status</label>--}}
-{{--                            <div class="d-flex align-items-start">--}}
-{{--                                <label for="active">--}}
-{{--                                    <input type="checkbox" checked id="active"> Active--}}
-{{--                                </label>--}}
-{{--                                <label for="pending">--}}
-{{--                                    <input type="checkbox" id="pending"> Pending/Under Contract--}}
-{{--                                </label>--}}
-{{--                            </div>--}}
-{{--                            <label>Property Type</label>--}}
+                    {{--                    <select name="price_range" id="" class="form-control select2">--}}
+                    {{--                        <option value="">Price Range</option>--}}
+                    {{--                    </select>--}}
+                    {{--                    <div class="dropdown">--}}
+                    {{--                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"--}}
+                    {{--                                aria-haspopup="true" aria-expanded="false">--}}
+                    {{--                            <i class="fas fa-sliders-h"></i> More Filters--}}
+                    {{--                        </button>--}}
+                    {{--                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">--}}
+                    {{--                            <label>Square Feet</label>--}}
+                    {{--                            <div class="d-flex align-items-center">--}}
+                    {{--                                <select name="" id="" class="form-control">--}}
+                    {{--                                    <option value="">Any Beds</option>--}}
+                    {{--                                </select>--}}
+                    {{--                                <span>to</span>--}}
+                    {{--                                <select name="" id="" class="form-control">--}}
+                    {{--                                    <option value="">Any Beds</option>--}}
+                    {{--                                </select>--}}
+                    {{--                            </div>--}}
+                    {{--                            <label>Lot Size</label>--}}
+                    {{--                            <div class="d-flex align-items-center">--}}
+                    {{--                                <select name="" id="" class="form-control">--}}
+                    {{--                                    <option value="">Any Beds</option>--}}
+                    {{--                                </select>--}}
+                    {{--                                <span>to</span>--}}
+                    {{--                                <select name="" id="" class="form-control">--}}
+                    {{--                                    <option value="">Any Beds</option>--}}
+                    {{--                                </select>--}}
+                    {{--                            </div>--}}
+                    {{--                            <label>Status</label>--}}
+                    {{--                            <div class="d-flex align-items-start">--}}
+                    {{--                                <label for="active">--}}
+                    {{--                                    <input type="checkbox" checked id="active"> Active--}}
+                    {{--                                </label>--}}
+                    {{--                                <label for="pending">--}}
+                    {{--                                    <input type="checkbox" id="pending"> Pending/Under Contract--}}
+                    {{--                                </label>--}}
+                    {{--                            </div>--}}
+                    {{--                            <label>Property Type</label>--}}
 
-{{--                            <div class="d-flex">--}}
-{{--                                <label for="single">--}}
-{{--                                    <input type="checkbox" checked id="single"> Single Family--}}
-{{--                                </label>--}}
-{{--                                <label for="Condo">--}}
-{{--                                    <input type="checkbox" id="Condo"> Condo/Townhouse--}}
-{{--                                </label>--}}
-{{--                                <label for="multi">--}}
-{{--                                    <input type="checkbox" id="multi"> Multi Family--}}
-{{--                                </label>--}}
-{{--                                <label for="land">--}}
-{{--                                    <input type="checkbox" id="land"> Land--}}
-{{--                                </label>--}}
-{{--                            </div>--}}
-{{--                            <div>--}}
-{{--                                <button type="button" class="btn themeBtn btn1">View Advanced Filters</button>--}}
-{{--                            </div>--}}
-{{--                            <div class="d-flex">--}}
-{{--                                <button type="button" class="btn themeBtn">Reset Filters</button>--}}
-{{--                                <button type="button" class="btn themeBtn btn3">View Listings</button>--}}
+                    {{--                            <div class="d-flex">--}}
+                    {{--                                <label for="single">--}}
+                    {{--                                    <input type="checkbox" checked id="single"> Single Family--}}
+                    {{--                                </label>--}}
+                    {{--                                <label for="Condo">--}}
+                    {{--                                    <input type="checkbox" id="Condo"> Condo/Townhouse--}}
+                    {{--                                </label>--}}
+                    {{--                                <label for="multi">--}}
+                    {{--                                    <input type="checkbox" id="multi"> Multi Family--}}
+                    {{--                                </label>--}}
+                    {{--                                <label for="land">--}}
+                    {{--                                    <input type="checkbox" id="land"> Land--}}
+                    {{--                                </label>--}}
+                    {{--                            </div>--}}
+                    {{--                            <div>--}}
+                    {{--                                <button type="button" class="btn themeBtn btn1">View Advanced Filters</button>--}}
+                    {{--                            </div>--}}
+                    {{--                            <div class="d-flex">--}}
+                    {{--                                <button type="button" class="btn themeBtn">Reset Filters</button>--}}
+                    {{--                                <button type="button" class="btn themeBtn btn3">View Listings</button>--}}
 
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
+                    {{--                            </div>--}}
+                    {{--                        </div>--}}
+                    {{--                    </div>--}}
 
                     <div class="ml-auto">
                         <button class="clear-filter-btn" onclick="clearSearchValue()">Clear Filter</button>
@@ -131,7 +132,6 @@
                     <div class="row">
 
                         @foreach($properties as $property)
-
                             {{--                        <a href="{{route('propertyDetail' , $property->id)}}">--}}
                             <div class="col-md-3 mb-4"
                                  onclick="window.location.href = '{{ route('propertyDetail' , $property->id )}}'">
@@ -140,9 +140,13 @@
                                         <span class="badge badge-primary">Feature</span>
                                         <span class="badge badge-secondary">For Rent</span>
                                     </div>
-                                    <img
-                                        src="{{$property->hasMedia('images') ? $property->getFirstMedia('images')->getUrl() : asset('images/single-property-01.jpg')}}"
-                                        alt="">
+                                    @foreach($property->media as $media)
+                                        <img
+                                            src="{{ !empty($media->first())
+                                            ? $media->getUrl()
+                                            : asset('images/single-property-01.jpg') }}"
+                                            alt="">
+                                    @endforeach
                                     <div class="card-footer">
                                         <a href="#" class="p">{{ $property->property_address ?? '' }}</a>
                                         <a href="#" class="h5 m-0">{{ $property->property_name ?? '' }}</a>
